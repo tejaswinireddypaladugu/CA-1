3a28b280685b3e2984df7d1408110fc5ec6e0d57
tejaswinireddypaladugu <tejaswinireddy989@gmail.com>
2024-07-15 01:17:15 +0530

    ContactUs.js

---
+++ ContactUs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ContactUs = () => {
+    return (
+        <div className="container">
+            <h2>Contact Us</h2>
+            <p>Email: contact@sunshinebakery.com</p>
+            <p>Phone: +123 456 7890</p>
+            <p>Address: 123 Sunshine St, Baker City, CA 12345</p>
+        </div>
+    );
+};
+
+export default ContactUs;
